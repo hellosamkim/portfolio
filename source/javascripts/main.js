2300c83ac4866ee264acbd4c6286a0e92250f56d
hellosamkim <sam.mt.kim@gmail.com>
2015-03-26 07:18:10 -0400

    Finished abouts page

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $('#site-wrapper').hide();
-  setTimeout("$('#site-wrapper').fadeIn(3000)", 5100);
+  // $('#site-wrapper').hide();
+  // setTimeout("$('#site-wrapper').fadeIn(3000)", 5100);
   $('#first-phrase').typed({
     strings: ['Hi There!', 'You can call me Sam.', 'Sam Kim'],
     typeSpeed: 4,
@@ -18,8 +18,8 @@ $(document).ready(function(){
     startDelay: 7500,
     backDelay: 1000
   });
-  $('#site-details').hide();
-  setTimeout("$('#site-details').fadeIn(1000)", 9000);
+  // $('#site-details').hide();
+  // setTimeout("$('#site-details').fadeIn(1000)", 9000);
   $('#my-statements').typed({
     strings: ['I can play 5 Instruments?', 'I used to be a DJ?', 'I have a dog named Coco?', 'I can Beatbox?', 'I can speak fluent Korean?', "The first programming language I learnt was Pascal?", "A can of Diet Coke floats in water, but Regular Coca-Cola sinks? Try it!"],
     typeSpeed: 20,
@@ -29,16 +29,14 @@ $(document).ready(function(){
   loadPage('abouts')
   loadPage('contact')
   loadPage('portfolio')
+  $('#made').hide()
 
   function loadPage(page){
     $('#' + page).click(function(e){
       e.preventDefault();
-      $('#my-info').html(page).fadein('slow')
+      $('#my-info').load(page + ' #' + page, function(){
+        $(this).hide().fadeIn('slow');
+      });
     });
   };
 });
-
-// $('#abouts').click(function(e){
-//     e.preventDefault();
-//     $('#my-info').load('abouts')
-//   });
