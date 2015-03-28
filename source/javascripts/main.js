d30133d669ddec377f86de2ddc4ae6286150dfbb
hellosamkim <sam.mt.kim@gmail.com>
2015-03-27 20:32:22 -0400

    heroku deploy

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  // $('#site-wrapper').hide();
-  // setTimeout("$('#site-wrapper').fadeIn(3000)", 5100);
+  $('#site-wrapper').hide();
+  setTimeout("$('#site-wrapper').fadeIn(3000)", 5100);
   $('#first-phrase').typed({
     strings: ['Hi There!', 'You can call me Sam.', 'Sam Kim'],
     typeSpeed: 4,
@@ -18,8 +18,8 @@ $(document).ready(function(){
     startDelay: 7500,
     backDelay: 1000
   });
-  // $('#site-details').hide();
-  // setTimeout("$('#site-details').fadeIn(1000)", 9000);
+  $('#site-details').hide();
+  setTimeout("$('#site-details').fadeIn(1000)", 9000);
   $('#my-statements').typed({
     strings: ['I can play 5 Instruments?', 'I used to be a DJ?', 'I have a dog named Coco?', 'I can Beatbox?', 'I can speak fluent Korean?', "The first programming language I ever learnt was Pascal?", "A can of Diet Coke floats in water, but Regular Coca-Cola sinks? Try it!"],
     typeSpeed: 20,
